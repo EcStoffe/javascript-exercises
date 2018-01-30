edb2fbd1060a6b9ea5ff63701b10ed69b0f2d2b3
Christoffer <jo.christoffer@gmail.com>
2018-01-30 11:41:03 +0100

    nya övningar avklarade

---
+++ dom-exercises/scripts/index.js
@@ -0,0 +1,29 @@
+/*function init() {
+    about = document.getElementById("about");
+    about.style.color = 'blue';
+    about.style.fontSize = '28px';
+}*/
+
+/*let myButton = document.getElementById('submit')
+
+myButton.addEventListener('click', function(e) {
+    let firstName = document.getElementById('fname');
+    let lastName = document.getElementById('lname');
+
+    console.log("Name: ", firstName.value, lastName.value)
+});*/
+
+
+/*let myButton = document.getElementById('submit')
+
+myButton.addEventListener('click', function (e) {
+    let myObject = document.getElementById("myHref");
+
+    let hrefLang = myObject.getAttribute("hreflang");
+    let href = myObject.getAttribute("href");
+    let target = myObject.getAttribute("target");
+    let myHref = myObject.getAttribute("id");
+
+    console.log(href, hrefLang, target, myHref);
+});*/
+
