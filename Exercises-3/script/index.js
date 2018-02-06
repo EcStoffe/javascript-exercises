3ea1661dc6a47787104fc0c59514083c8133f169
Christoffer <jo.christoffer@gmail.com>
2018-02-06 10:35:10 +0100

    Övning 3, klar

---
+++ Exercises-3/script/index.js
@@ -0,0 +1,10 @@
+let valueOne = parseInt(prompt("Add a number: "));
+let valueTwo = parseInt(prompt("Add a second number: "));
+console.log(valueOne, valueTwo);
+
+//Stretch task
+
+/*
+let numbers = prompt("add two numbers").split(" ");
+
+console.log(numbers);*/
